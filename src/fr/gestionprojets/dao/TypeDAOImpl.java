928e21ea806f4ecd53f81b4eb08d69de6137fe58
unknown <khaledboussaba@gmail.com>
2019-08-06 01:41:07 +0200

    Couche DAO Type OK

---
+++ src/fr/gestionprojets/dao/TypeDAOImpl.java
@@ -0,0 +1,46 @@
+package fr.gestionprojets.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import fr.gestionprojets.dao.entity.Type;
+import fr.gestionprojets.utils.HibernateUtil;
+
+public class TypeDAOImpl implements TypeDao {
+
+	private Session session = HibernateUtil.openSession();
+
+	@Override
+	public void add(Type type) {
+		session.beginTransaction();
+		session.save(type);
+		session.getTransaction().commit();
+	}
+	
+	@Override
+	public Type findById(Long id) {
+		return (Type) session.get(Type.class, id);
+	}
+
+	@Override
+	public Type edit(Type type) {
+		session.beginTransaction();
+		Type t = (Type) session.merge(type);
+		session.getTransaction().commit();
+		return t;
+	}
+
+	@Override
+	public void delete(Long id) {
+		session.beginTransaction();
+		Type t = findById(id);
+		session.delete(t);
+		session.getTransaction().commit();
+	}
+
+	@Override
+	public List<Type> finAll() {
+		return session.createQuery("SELECT t FROM Type t").list();
+	}
+}
---
+++ src/fr/gestionprojets/dao/TypeDao.java
@@ -0,0 +1,17 @@
+package fr.gestionprojets.dao;
+
+import java.util.List;
+
+import fr.gestionprojets.dao.entity.Type;
+
+public interface TypeDao {
+	void add(Type type);
+	
+	Type edit(Type type);
+	
+	void delete(Long id);
+	
+	List<Type> finAll();
+	
+	Type findById(Long id);
+}
